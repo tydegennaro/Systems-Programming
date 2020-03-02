02b88ad876b8408c17017cb3944da5d19b18fd17
kyledegennaro <kyle.degennaro@gmail.com>
2020-03-02 15:17:14 -0500

    Add comments

--- Assignments/prog1/mybackup.sh
+++ Assignments/prog1/mybackup.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+#   CSC 399: Systems Programming
+
+# Program Name: 
+#   mybackup.sh
+
+# Program Purpose:
+#   This script takes two integers as arguments, 
+#   and prints all prime numbers from `START_NUMBER` ($1) to `END_NUMBER` ($2)
+
+# Author(s):
+#   Kyle DeGennaro
+#   Tyler DeGennaro
+#   Joey Germain
+
+
+
 # Validate input
 if [ "$#" -gt "1" ]; then
     echo "USAGE: mybackup.sh [AGE]"
